3741b495699962cd7487b4504c4b7dbb3ac5ed8e
Gulamnabi Kagathala <145833927+gulu43@users.noreply.github.com>
2025-03-05 08:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 /* Edit this file */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+let player = document.querySelector('.player');
+let video = player.querySelector('.viewer');
+let progress = player.querySelector('.progress');
+let progressBar = document.querySelector('.progress__filled');
+// let pb = document.querySelector('#pb');
+let toggle = player.querySelector('.toggle');
+let skipButtons = player.querySelectorAll('[data-skip]');
+let ranges = player.querySelectorAll('.player__slider');
+let volume_var = player.querySelector('.volume');
+let v_speed_var = player.querySelector('.v_speed');
+let pre_var = player.querySelector('.pre');
+let pos_var = player.querySelector('.pos');
+
+
+// toggle part
+toggle.addEventListener('click', ()=>{
+    if (video.paused) {
+        video.play();
+        toggle.textContent="❚ ❚";
+    }else{
+        video.pause();
+        toggle.textContent="►";
+    }
+});
+
+// progess bar
+video.addEventListener('timeupdate',(e)=>{   
+    let curP = ((video.currentTime / video.duration) * 100);
+    progressBar.style.width = `${curP}%`;
+});
+
+// volume
+volume_var.addEventListener('change',(e)=>{
+    video.volume = e.target.value;
+    console.log(video.volume);
+});
+
+// seekbar
+v_speed_var.addEventListener('change',(e)=>{
+    video.playbackRate = parseFloat(e.target.value);
+    console.log(video.playbackRate);
+});
+// buttons
+pre_var.addEventListener('click',(e)=>{
+    video.currentTime += parseFloat(pre_var.dataset.skip);
+});
+pos_var.addEventListener('click',(e)=>{
+    video.currentTime += parseFloat(pos_var.dataset.skip);
+});
